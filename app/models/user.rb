0676bca7e074ca33ec4fadd5d5fd316d8d98f370
athanael gerard <pilotewpa@gmail.com>
2024-10-12 13:08:16 +0200

    remove my fake user

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  has_many :arena
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true
-  validates :email, uniqueness: true
-  has_one_attached :photo
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_11_112547) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_12_110635) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_11_112547) do
   end
 
   create_table "arenas", force: :cascade do |t|
-    t.bigint "user_id", null: false
     t.string "name"
     t.integer "capacity"
     t.string "address"
@@ -51,18 +50,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_11_112547) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
-    t.index ["user_id"], name: "index_arenas_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "arenas", "users"
 end
