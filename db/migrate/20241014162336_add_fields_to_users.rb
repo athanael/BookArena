f827c3d4b98e09bb725a4162449ca00795966460
athanael gerard <pilotewpa@gmail.com>
2024-10-14 20:14:02 +0200

    last migration was useless

--- db/migrate/20241014162336_add_fields_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddFieldsToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
-    add_column :users, :category, :string
-  end
-end
