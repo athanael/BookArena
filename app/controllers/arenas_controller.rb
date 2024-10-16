c0ce06c683f2b70b9f4166446a64afa401383387
athanael gerard <pilotewpa@gmail.com>
2024-10-16 11:32:00 +0200

    On peut créer une nouvelle salle, la redirection si personne n'est connecté fonctionne

--- app/controllers/arenas_controller.rb
+++ app/controllers/arenas_controller.rb
@@ -1,9 +1,38 @@
 class ArenasController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
     @arenas = Arena.all
   end
 
   def show
     @arena = Arena.find(params[:id])
+  end
+
+  def new
+    @arena = Arena.new
+  end
+
+  def create
+    if current_user.nil?
+      flash[:alert] = "Vous devez être connecté pour créer une arène."
+      redirect_to new_user_session_path and return
+    end
+
+    @arena = Arena.new(arena_params)
+    @arena.user = current_user
+
+    if @arena.save
+      redirect_to arena_path(@arena), notice: 'La salle a été créée avec succès.'
+    else
+      logger.error "Erreur lors de la création de l'arène : #{@arena.errors.full_messages.join(', ')}"
+      render :new
+    end
+  end
+
+
+  private
+
+  def arena_params
+    params.require(:arena).permit(:name, :capacity, :address, :price, :description, :photo)
   end
 end
