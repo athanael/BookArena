62d0a552e0a9f2b27734c4c68911b781a07576f9
athanael gerard <pilotewpa@gmail.com>
2024-10-23 08:23:49 +0200

    test

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,11 @@ Rails.application.routes.draw do
   # Page d'accueil
   root to: 'pages#home'
 
-<<<<<<< HEAD
   resources :arenas, only: [:index, :show, :new, :create, :destroy]
   # Defines the root path route ("/")
   # root "posts#index"
-=======
   # Routes pour les arènes avec les actions index et show
   resources :arenas do
     resources :bookings, only: [:index, :show, :new, :create]
   end
->>>>>>> 8050e72c8d5cb3013f0ecd6ba4b4c66a5afbcd57
 end
